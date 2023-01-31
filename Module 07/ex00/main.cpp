26fa49979af879f9da7669ad8265f74648521f7a
Marce <lightsxworld@gmail.com>
2023-01-31 16:11:33 -0300

    Module 07 ex00 done

---
+++ Module 07/ex00/main.cpp
@@ -0,0 +1,38 @@
+#include "whatever.hpp"
+
+int main( void )
+{
+
+  std::cout << std::endl;
+
+  int a = 2;
+  int b = 3;
+
+  std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
+
+  ::swap( a, b );
+
+  std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
+
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+  std::cout << std::endl;
+
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+
+  std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
+
+  ::swap(c, d);
+
+  std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
+
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+  std::cout << std::endl;
+
+  return (0);
+
+}
