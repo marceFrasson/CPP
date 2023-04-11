8de786cfe1f2518b0586c5f61c33ed4b0a14bd78
Marce <lightsxworld@gmail.com>
2023-04-11 19:20:10 -0300

    workspace fixes

--- Module 07/ex01/main.cpp
+++ Module 07/ex01/main.cpp
@@ -3,14 +3,38 @@
 int main( void )
 {
 
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int length = sizeof(array) / sizeof(array[0]);
+	{
+		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int length = sizeof(array) / sizeof(array[0]);
 
-  std::cout << std::endl;
+		std::cout << std::endl << "Int   array: ";
 
-  iter(array, length, printElement<int>);
+		iter(array, length, printElement<int>);
 
-  std::cout << std::endl << std::endl;
+		std::cout << std::endl;
+	}
+
+	{
+		float array[] = {1.4, 3.2, 5.9, 7.8, 9, 10};
+		int length = sizeof(array) / sizeof(array[0]);
+
+		std::cout << std::endl << "Float array: ";
+
+		iter(array, length, printElement<float>);
+
+  		std::cout << std::endl;
+	}
+
+	{
+		char array[] = {'z', 'e', 'g', 'r', 't', 'u', 'y', 'l', 'a', 'q', 'i'};
+		int length = sizeof(array) / sizeof(array[0]);
+
+		std::cout << std::endl << "Char  array: ";
+
+		iter(array, length, printElement<char>);
+
+  		std::cout << std::endl << std::endl;
+	}
 
   return (0);
 
